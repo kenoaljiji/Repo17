d9e0a46e1053cac105de89366a26b15c01ed2a48
kenoaljiji <studioaljiji@gmail.com>
2021-09-29 18:48:41 +0200

    Replace ES5 to destructuring and add variable new name

---
+++ ex03/src/index.js
@@ -0,0 +1,21 @@
+const MY_CAR = {
+  wheels: 4,
+  cylinders: 4,
+  size: 2.2,
+};
+
+function main(myCar) {
+  //Only change code below this line
+
+  const { cylinders: carCylinders, size: carSize } = myCar;
+
+  //Only change code above this line
+
+  return {
+    carCylinders,
+    carSize,
+  };
+}
+
+console.log(main(MY_CAR));
+module.exports = main;
